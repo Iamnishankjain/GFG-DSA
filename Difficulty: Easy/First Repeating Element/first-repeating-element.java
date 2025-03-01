f2c12e7a7c9c59ad2add27651993b17e0cf1e6ca
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-03-02 00:01:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Repeating Element/first-repeating-element.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Long> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Long.parseLong(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (long i : array) arr[idx++] = (int)i;
+
+            Solution obj = new Solution();
+
+            // calling maxSubarraySum() function
+            System.out.println(obj.firstRepeated(arr));
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to return the position of the first repeating element.
+    public static int firstRepeated(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int n=arr.length;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            if(map.containsKey(arr[i])){
+                min=Math.min(min,map.get(arr[i]));
+            }
+                map.put(arr[i],i+1);
+        }
+        if(min!=Integer.MAX_VALUE) return min;
+        return -1;
+    }
+}
