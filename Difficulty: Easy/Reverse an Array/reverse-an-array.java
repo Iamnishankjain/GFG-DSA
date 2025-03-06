412ecea0b149b4b439bd465d9f6dbd66558ece0b
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-03-07 00:18:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        while (tc-- > 0) {
+            String str[] = br.readLine().split(" ");
+            int arr[] = new int[str.length];
+
+            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
+            Solution obj = new Solution();
+
+            obj.reverseArray(arr);
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public void reverseArray(int arr[]) {
+        reverse(arr,0,arr.length-1);
+    }
+    
+    public void reverse(int arr[],int lb,int ub){
+        if(lb>ub){
+            return;
+        }
+        int temp=arr[lb];
+        arr[lb]=arr[ub];
+        arr[ub]=temp;
+        reverse(arr,lb+1,ub-1);
+    }
+}
