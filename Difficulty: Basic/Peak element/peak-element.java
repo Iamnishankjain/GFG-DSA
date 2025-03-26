273b25a64f323a91ccb937d60a5a0171a4b10761
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-03-26 22:52:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Peak element/peak-element.java
+++ Difficulty: Basic/Peak element/peak-element.java
@@ -55,9 +55,16 @@ class Solution {
         if (n == 1) return 0;
         if (arr[0] > arr[1]) return 0;
         if (arr[n - 1] > arr[n - 2]) return n - 1;
-        for(int i=1;i<n-1;i++){
-            if(arr[i]>arr[i+1] && arr[i]>arr[i-1]){
-                return i;
+        int lb=1;
+        int ub=n-2;
+        while(lb<=ub){
+            int mid=lb+(ub-lb)/2;
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]) return mid;
+            else if(arr[mid]>arr[mid+1]){
+                ub=mid-1;
+            }
+            else{
+                lb=mid+1;
             }
         }
         return -1;
