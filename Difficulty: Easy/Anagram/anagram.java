5a1c825f8759f3653e7031ed7552e92e01191816
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-04-23 00:25:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import java.util.stream.*;
+
+class GFG {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String s1 = br.readLine(); // first string
+            String s2 = br.readLine(); // second string
+
+            Solution obj = new Solution();
+
+            if (obj.areAnagrams(s1, s2)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function is to check whether two strings are anagram of each other or not.
+    public static boolean areAnagrams(String s, String t) {
+
+        if(s.length()!=t.length()) return false;
+        int arr1[]=new int[26];
+        int arr2[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            arr1[s.charAt(i)-'a']++;
+            arr2[t.charAt(i)-'a']++;
+        }
+
+        for(int i=0;i<26;i++){
+            if(arr1[i]!=arr2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
