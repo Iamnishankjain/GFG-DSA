fafc4612af6dd201c81e0828cd1470beee0279e3
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-06-07 21:39:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Generate all binary strings/generate-all-binary-strings.java
+++ Difficulty: Medium/Generate all binary strings/generate-all-binary-strings.java
@@ -3,11 +3,11 @@
 class Solution {
     public static List<String> generateBinaryStrings(int n) {
         List<String> list=new ArrayList<>();
-        valid(list,n,n,"",n,-1);
+        valid(list,"",n,-1);
         return list;
     }
 
-    public static void valid(List<String> list,int one,int zero,String s,int n,int flag){
+    public static void valid(List<String> list,String s,int n,int flag){
         if(s.length()>n){
             return;
         }
@@ -16,8 +16,8 @@ class Solution {
             s="";
             return;
         }
-        valid(list,one,zero-1,s+'0',n,0);
+        valid(list,s+'0',n,0);
         if(flag!=1)
-        valid(list,one-1,zero,s+'1',n,1);
+        valid(list,s+'1',n,1);
     }
 }
