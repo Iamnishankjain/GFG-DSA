f089f3a5e697455cbf2622f6bda38af2fa0af675
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-07-08 23:11:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of 1 Bits/number-of-1-bits.java
+++ Difficulty: Easy/Number of 1 Bits/number-of-1-bits.java
@@ -3,7 +3,7 @@ class Solution {
     static int setBits(int n) {
         int c=0;
         while(n!=0){
-            if(n%2==1) c++;
+            c+=n&1;
             n/=2;
         }
         return c;
