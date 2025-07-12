d374d7431e842e643853fe51514a2c1b42de0b40
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-07-12 23:29:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Implement stack using array/implement-stack-using-array.java
+++ Difficulty: Basic/Implement stack using array/implement-stack-using-array.java
@@ -1,45 +1,3 @@
-//{ Driver Code Starts
-import java.util.*;
-
-// Define MyStack class here
-
-public class Main {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int T = scanner.nextInt();
-        scanner.nextLine(); // Flush the newline character left by nextInt
-        while (T-- > 0) {
-            MyStack sq = new MyStack();
-            String line = scanner.nextLine();
-            Scanner ss = new Scanner(line);
-            List<Integer> nums = new ArrayList<>();
-            while (ss.hasNextInt()) {
-                int num = ss.nextInt();
-                nums.add(num);
-            }
-            int n = nums.size();
-            int i = 0;
-            while (i < n) {
-                int QueryType = nums.get(i++);
-                if (QueryType == 1) {
-                    int a = nums.get(i++);
-                    sq.push(a);
-                } else if (QueryType == 2) {
-                    System.out.print(sq.pop() + " ");
-                }
-            }
-            System.out.println();
-        
-System.out.println("~");
-}
-        scanner.close();
-    }
-}
-
-// } Driver Code Ends
-
-
 class MyStack {
     private int[] arr;
     private int top;
@@ -50,19 +8,19 @@ class MyStack {
     }
 
     public void push(int x) {
-        if(top==arr.length-1){
-            return;
+        if(top<1000-1){
+            top++;
+            arr[top]=x;
         }
-        top++;
-        arr[top]=x;
     }
 
     public int pop() {
-        if(top==-1){
-            return -1;
+        int temp=-1;
+        if(top!=-1){
+            temp=arr[top];
+            arr[top]=-1;
+            top--;
         }
-        int item=arr[top];
-        top--;
-        return item;
+        return temp;
     }
 }
