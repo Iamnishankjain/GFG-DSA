62afb6cbb701e74a3e1188266d3c3f6a0a25db95
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-07-13 00:06:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Queue Using Array/queue-using-array.java
+++ Difficulty: Basic/Queue Using Array/queue-using-array.java
@@ -1,81 +1,38 @@
-//{ Driver Code Starts
-import java.util.Scanner;
-
-class GfG
-{
-	public static void main(String args[])
-	{
-		 Scanner sc = new Scanner(System.in);
-		 int t=sc.nextInt();
-		 while(t>0)
-		 {
-			MyQueue obj = new MyQueue();
-			int Q = sc.nextInt();
-			while(Q>0)
-			{
-				int QueryType = 0;
-				QueryType = sc.nextInt();
-				if(QueryType == 1)
-				{
-					int a = sc.nextInt();
-					
-					obj.push(a);
-					
-				}else if(QueryType == 2)
-				{
-				System.out.print(obj.pop()+" ");
-				}
-				Q--;
-			}
-			System.out.println("");
-			t--;
-		 
-System.out.println("~");
-}
-	}
-}
-
-
-
-
-// } Driver Code Ends
-
-
-
-
 class MyQueue {
 
     int front, rear;
-	int arr[] = new int[100005];
+    int arr[] = new int[100005];
+    int curr=0;
+    MyQueue() {
+        front = 0;
+        rear = 0;
+    }
 
-    MyQueue()
-	{
-		front=0;
-		rear=0;
-	}
-	
-	//Function to push an element x in a queue.
-	void push(int x)
-	{
-	    if(rear==arr.length){
-	        return;
-	    }
-	    arr[rear]=x;
-	    rear++;
-	} 
+    // Function to push an element x in a queue.
+    void push(int x) {
+        if(curr==0){
+            front = 0;
+            rear = -1;
+        }
+        rear=(rear+1)%100005;
+        arr[rear]=x;
+        curr++;
+    }
 
-    //Function to pop an element from queue and return that element.
-	int pop()
-	{
-		if(front==rear){
-		    return -1;
-		}
-		int temp=arr[front];
-		front++;
-		return temp;
-	} 
+    // Function to pop an element from queue and return that element.
+    int pop() {
+        if(curr==0){
+            return -1;
+        }
+        int temp=arr[front];
+        if(curr==1){
+            front=-1;
+            rear=-1;
+        }
+        else{
+            front=(front+1)%100005;
+        }
+        curr--;
+        return temp;
+    }
 }
-
-
-
-
