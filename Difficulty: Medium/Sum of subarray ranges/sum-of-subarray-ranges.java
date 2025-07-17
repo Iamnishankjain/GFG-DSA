bb5b5c59564b7c4493c8169e5e1319ce63013a32
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-07-17 23:29:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of subarray ranges/sum-of-subarray-ranges.java
@@ -0,0 +1,79 @@
+class Solution {
+    public int[] nextSmaller(int[] arr) {
+        int n = arr.length;
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) stack.pop();
+            res[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+        return res;
+    }
+
+    public int[] prevSmallerEqual(int[] arr) {
+        int n = arr.length;
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) stack.pop();
+            res[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        return res;
+    }
+
+    public long minSumSubarray(int[] arr) {
+        int n = arr.length;
+        int[] next = nextSmaller(arr);
+        int[] prev = prevSmallerEqual(arr);
+        long total = 0;
+        for (int i = 0; i < n; i++) {
+            long left = i - prev[i];
+            long right = next[i] - i;
+            total += left * right * 1L * arr[i];
+        }
+        return total;
+    }
+
+    public int[] nextGreaterEqual(int[] arr) {
+        int n = arr.length;
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) stack.pop();
+            res[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+        return res;
+    }
+
+    public int[] prevGreater(int[] arr) {
+        int n = arr.length;
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] <= arr[i]) stack.pop();
+            res[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        return res;
+    }
+
+    public long maxSumSubarray(int[] arr) {
+        int n = arr.length;
+        int[] next = nextGreaterEqual(arr);
+        int[] prev = prevGreater(arr);
+        long total = 0;
+        for (int i = 0; i < n; i++) {
+            long left = i - prev[i];
+            long right = next[i] - i;
+            total += left * right * 1L * arr[i];
+        }
+        return total;
+    }
+
+    public long subarrayRanges(int[] nums) {
+        return maxSumSubarray(nums) - minSumSubarray(nums);
+    }
+}
