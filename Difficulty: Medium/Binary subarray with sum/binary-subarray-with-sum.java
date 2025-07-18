5e8b8a2c15c94dd9c7f36492ba965563bd181126
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-07-19 01:32:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Binary subarray with sum/binary-subarray-with-sum.java
@@ -0,0 +1,23 @@
+// User function Template for Java
+
+class Solution {
+    public int numberOfSubarrays(int[] arr, int goal) {
+        // code here
+        return numSubarrayslessSum(arr,goal)-numSubarrayslessSum(arr,goal-1);
+    }
+    public int numSubarrayslessSum(int[] nums, int goal) {
+        if(goal<0) return 0;
+        int cnt=0,sum=0;
+        int l=0;
+        for(int r=0;r<nums.length;r++){
+            sum+=nums[r];
+            while(sum>goal){
+                sum-=nums[l];
+                l++;
+            }
+            if(sum<=goal)
+                cnt+=(r-l+1);
+        }
+        return cnt;
+    }
+}
