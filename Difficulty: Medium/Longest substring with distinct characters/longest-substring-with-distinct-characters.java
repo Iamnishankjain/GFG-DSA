1140af4fcdcb6e56883a122298ddbb0f2f57a417
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-07-18 22:56:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest substring with distinct characters/longest-substring-with-distinct-characters.java
@@ -0,0 +1,20 @@
+// User function Template for Java
+
+class Solution {
+    public int longestUniqueSubstr(String s) {
+        // code here
+        int hash[]=new int[256];
+        Arrays.fill(hash,-1);
+        int l=0;
+        int maxlen=0;
+        for(int r=0;r<s.length();r++){
+            if(hash[s.charAt(r)]!=-1){
+                if(hash[s.charAt(r)]>=l)
+                    l=hash[s.charAt(r)]+1;
+            }
+            maxlen=Math.max(maxlen,r-l+1);
+            hash[s.charAt(r)]=r;
+        }
+        return maxlen;
+    }
+}
