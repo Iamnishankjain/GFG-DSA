d23e18735d4cb560814691ee934cbbd755f63bb5
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-07-20 01:14:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.java
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.java
@@ -1,50 +1,18 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t;
-        t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            System.out.println(new Solution().minJumps(arr));
-            // System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    static int minJumps(int[] arr) {
-        int max=0,choice=0,jump=0;
-        for(int i=0;i<arr.length-1;i++){
-            max=Math.max(arr[i]+i,max);
-            if(choice==i){
-                choice=max;
-                jump++;
+    static int minJumps(int[] nums) {
+        // code here
+        int l=0,r=0,jump=0;
+        int n=nums.length;
+        while(r<n-1){
+            int far=r;
+            for(int i=l;i<=r;i++){
+                far=Math.max(far,i+nums[i]);
             }
+            if (far == r) return -1; 
+            l=r+1;
+            r=far;
+            jump++;
         }
-        return choice>=arr.length-1 ? jump : -1;
+        return jump;
     }
 }
