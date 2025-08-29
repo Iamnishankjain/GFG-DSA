963183e78f801235108a43a5ef714d823e210d6c
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-08-29 21:07:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Equilibrium Point/equilibrium-point.java
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.java
@@ -1,41 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
-        while (t-- > 0) {
-
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(
-                    token)); // Use Integer.parseInt to parse int values
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            Solution obj = new Solution();
-
-            // calling equilibriumPoint() function
-            System.out.println(obj.findEquilibrium(arr));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
 
 
 class Solution {
@@ -43,7 +5,7 @@ class Solution {
     public static int findEquilibrium(int arr[]) {
         int n=arr.length;
         if(n==1){
-            return arr[0];
+            return 0;
         }
         if(n==2){
             return -1;
