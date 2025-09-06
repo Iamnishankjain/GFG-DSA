0c65b3dbe577e6b34adefb73c71e7d95188f0651
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-09-06 23:33:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum number of deletions and insertions/minimum-number-of-deletions-and-insertions.java
@@ -0,0 +1,20 @@
+// User function Template for Java
+
+class Solution {
+    public int minOperations(String s1, String s2) {
+        int n1=s1.length();
+		int n2=s2.length();
+		int dp[]=new int[n2+1];
+		for(int i=1;i<=n1;i++){
+            int temp[]=new int[n2+1];
+			for(int j=1;j<=n2;j++){
+        		if(s1.charAt(i-1)==s2.charAt(j-1)) 
+        		   temp[j]= 1+dp[j-1];
+        		else
+        		    temp[j]= Math.max(dp[j],temp[j-1]);
+			}
+            dp=temp;
+		}
+		return n1-dp[n2]+n2-dp[n2];
+    }
+}
